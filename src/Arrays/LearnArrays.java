95aa732f653f30b4ed475e603c1580a54369a1e7
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-15 11:30:15 +0700

    array1

---
+++ src/Arrays/LearnArrays.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class LearnArrays {
+
+//        bài 1:  in ra các phần tử trùng nhau trong mảng
+
+//    public static int[] getElementSame(int[] a) {
+//        int[] a = new int[] {1, 2, 3, 4, 2, 7, 8, 8, 3};
+//        System.out.println("Các phần tử trùng nhau trong mảng là : ");
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = i + 1; j < a.length; j++) {
+//                if (a[i] == a[j]) {
+//                    System.out.println(a[j]);
+//
+//                }
+//            }
+//        }
+//        return
+//    }
+
+    public static int sum(int a, int b){
+    return a+b;
+}
+    public static void main(String[] args) {
+        int result = sum(2,3);
+        System.out.println("Ket qua là : " + result);
+    }
+
+    }
+
+
+
