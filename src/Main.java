27305925e9326fff4811f835eba7920b62e6a2e8
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-15 09:44:45 +0700

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //bài 1:  in ra các phần tử trùng nhau trong mảng
+
+//        int[] a = new int[] {1, 2, 3, 4, 2, 7, 8, 8, 3};
+//        System.out.println("Các phần tử trùng nhau trong mảng là : ");
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = i + 1; j < a.length; j++) {
+//                if (a[i] == a[j]) {
+//                    System.out.println(a[j]);
+//
+//                }
+//            }
+//        }
+
+        
     }
+
 }
