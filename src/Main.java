d49acb8a025e63d45effd09a7b30ef5350262009
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-15 13:24:33 +0700

    arrays new

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,6 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
-        //bài 1:  in ra các phần tử trùng nhau trong mảng
-
-//        int[] a = new int[] {1, 2, 3, 4, 2, 7, 8, 8, 3};
-//        System.out.println("Các phần tử trùng nhau trong mảng là : ");
-//        for (int i = 0; i < a.length; i++) {
-//            for (int j = i + 1; j < a.length; j++) {
-//                if (a[i] == a[j]) {
-//                    System.out.println(a[j]);
-//
-//                }
-//            }
-//        }
-
-        
-    }
 
 }
