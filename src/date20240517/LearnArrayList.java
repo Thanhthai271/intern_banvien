2823ff901d83a6dfbe84d554f58596af91e4f1e0
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-17 13:20:01 +0700

    NewArrayList

--- src/date20240517/LearnArrayList.java
+++ src/date20240517/LearnArrayList.java
@@ -5,26 +5,50 @@ import java.util.Scanner;
 
 public class LearnArrayList {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList <Double> input = new ArrayList<>();
 
-        while(true){
-            System.out.print("Nhập số : ");
-            double number = scanner.nextInt();
-            input.add(number);
-            scanner.nextLine();
+        // Khởi tạo một ArrayList để lưu trữ các số nguyên
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-            System.out.println("continue ? Y / N ? ");
-            String options = scanner.nextLine();
-            if(options.equals("N") || options.equals("n")){
-                break;
-            }
+        // Thêm các phần tử vào ArrayList
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        // Hiển thị các phần tử trong ArrayList
+        System.out.println("Các phần tử trong ArrayList:");
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
         }
-        System.out.println("Check Array " + input);
-        double sum = 0;
-        for(int i = 0 ; i < input.size(); i++){
-            sum = sum + input.get(i);
+
+        // Xóa một phần tử từ ArrayList
+        numbers.remove(1);
+
+        // Hiển thị lại các phần tử sau khi xóa
+        System.out.println("Các phần tử sau khi xóa:");
+        for (int number : numbers) {
+            System.out.println(number);
         }
-        System.out.println("Tổng các số là : " + sum);
+
+//        Scanner scanner = new Scanner(System.in);
+//        ArrayList <Double> input = new ArrayList<>();
+//
+//        while(true){
+//            System.out.print("Nhập số : ");
+//            double number = scanner.nextInt();
+//            input.add(number);
+//            scanner.nextLine();
+//
+//            System.out.println("continue ? Y / N ? ");
+//            String options = scanner.nextLine();
+//            if(options.equals("N") || options.equals("n")){
+//                break;
+//            }
+//        }
+//        System.out.println("Check Array " + input);
+//        double sum = 0;
+//        for(int i = 0 ; i < input.size(); i++){
+//            sum = sum + input.get(i);
+//        }
+//        System.out.println("Tổng các số là : " + sum);
     }
 }
