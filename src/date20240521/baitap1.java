63159461fa14bf7c087a147a29d0e7623a052ba2
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-21 16:33:07 +0700

    baitap1

---
+++ src/date20240521/baitap1.java
@@ -0,0 +1,32 @@
+package date20240521;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class baitap1 {
+    public static int [] arrange (int [] a){
+        for (int i = 0; i < a.length - 1; i++ ){
+            for(int j = i + 1; j < a.length; j++){
+                if (a[i] > a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap vao do dai cua mang : ");
+        int n = scanner.nextInt();
+        System.out.println("Nhap vao gia tri cua mang : ");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        int [] b = arrange(arr);
+        System.out.println(Arrays.toString(b));
+    }
+}
