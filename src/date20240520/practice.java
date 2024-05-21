825aecc96defada128522a00e618757acba0c316
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-21 14:36:00 +0700

    bai10

--- src/date20240520/practice.java
+++ src/date20240520/practice.java
@@ -8,20 +8,24 @@ public class practice {
 //     Viết hàm liệt kê các giá trị chẵn trong mảng 1 chiều các số nguyên
 
     public static int arr (int [] a){
-        int positiveNumber = 0;
-        for (int i = 0; i < a.length; i++){
-            if(a[i] > 0){
-                positiveNumber = a[i];
-                break;
-            }else
-                positiveNumber = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) {
+                return a[i];
+            }
         }
-
-        return positiveNumber;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int b = arr(new int[]{2,-2,-3,-4,-5,-6});
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao do dai cua mang : ");
+        int n = scanner.nextInt();
+        System.out.println("Nhap vao gia tri cua mang : ");
+        int [] ar = new int[n];
+        for (int i = 0; i < n; i++){
+            ar [i] = scanner.nextInt();
+        }
+        int b = arr(ar);
         System.out.println("Số nguyên dương đầu tiên trong mảng là : " + b);
     }
 
