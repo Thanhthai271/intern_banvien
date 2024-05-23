f91317815223d8091a435dfb7f9e588d33f90eab
Thanhthai27 <thanhthai2712002@gmail.com.vn>
2024-05-23 15:11:58 +0700

    baitap1va2

--- src/date20240523/baitap1.java
+++ src/date20240523/baitap1.java
@@ -7,17 +7,30 @@ public class baitap1 {
 
     //    4. Thêm 1 phần tử x vào mảng tại vị trí k
 
-    public static int[] add (int[] arr , int k){
+    public static int[] add (int[] arr , int x){
         int[] brr = new int[arr.length  + 1];
         for (int i = 0; i < arr.length;i++){
             brr[i] = arr[i];
         }
-        brr[brr.length - 1] = k;
+        brr[brr.length - 1] = x;
         return brr;
     }
+    public static void affterchange (int[] arr, int k,int x){
 
+        if (k >= 0 && k < arr.length) {
+            int[] newarrays = new int[arr.length];
+            for (int i = 0; i < arr[k]; i++) {
+                newarrays[i] = arr[i];
+            }
+            newarrays[k] = x;
+            for (int i = k + 1; i < arr.length; i++) {
+                newarrays[i] = arr[i - 1];
+            }
+            System.out.println(Arrays.toString(newarrays));
+        }
+    }
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao do dai cua mang : ");
         int n = scanner.nextInt();
         System.out.println("nhap vao gia tri cua mang : ");
@@ -26,9 +39,13 @@ public class baitap1 {
             arr[i] = scanner.nextInt();
         }
         System.out.println("nhap vao gia tri can them");
+        int x = scanner.nextInt();
+        arr = add(arr,x);
+        System.out.println("nhap vao vi tri k : ");
         int k = scanner.nextInt();
-        arr = add(arr,k);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Mảng sau khi thay đổi là : ");
+        affterchange(arr,k,x);
     }
+
 }
 
--- src/date20240523/baitap2.java
+++ src/date20240523/baitap2.java
@@ -1,5 +1,36 @@
 package date20240523;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class baitap2 {
-    
+
+//    Viết hàm nhập mảng sao cho khi nhập xong thì giá trị trong mảng sắp xếp giảm dần
+
+    // hàm sap xep giảm dần
+    public static void sortDescending (int[] firstarr){
+        for (int i = 0; i < firstarr.length - 1; i++){
+            for (int j = i + 1; j < firstarr.length; j++){
+                if(firstarr[i] < firstarr[j]){
+                    int temp = firstarr[i];
+                    firstarr[i] = firstarr[j];
+                    firstarr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap vao do dai cua mang : ");
+        int n = scanner.nextInt();
+        System.out.println("nhap vao gia tri cua mang : ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        sortDescending(arr);
+        System.out.println("Mảng sau khi đã sắp xếp la : ");
+        System.out.println(Arrays.toString(arr));
+    }
 }
